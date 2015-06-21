ce79702ea79123695fece555be81043c4c82591e
natsu12 <natsu12@126.com>
2015-06-21 21:02:52 +0800

    fix

--- data/import.sh
+++ data/import.sh
@@ -1,3 +1,3 @@
 mongoimport --db activitee --collection activities --file activities.json
-mongoimport --db activitee --collection users --file users_.json
+mongoimport --db activitee --collection users --file users.json
 mongoimport --db activitee --collection tags --file tags.json
